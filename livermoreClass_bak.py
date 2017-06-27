3e8445feb0731dfedda4e2fa4e1f89238d49ee98
ipqhjjybj <250657661@qq.com>
2017-06-27 14:25:10 +0800

    livermore has

--- livermoreClass_bak.py
+++ livermoreClass_bak.py
@@ -35,10 +35,6 @@ HeiMoShui      = "k"        # 上升趋势黑墨水
 HongMoShui     = "r"        # 下降趋势红墨水
 QianBi         = "b"		# 其他栏 铅笔
 
-NoBelowLine    = ""			# 没有下划线 
-RedBelowLine   = "r"        # 有红色的下划线
-BlackBelowLine = "b"        # 有黑色的下划线
-
 RED_LINE       = "r"        # 上升趋势以及自然回调后的点下标记
 BLACK_LINE     = "b"        # 下降趋势以及自然回升形成的点下标记
 
@@ -71,17 +67,17 @@ class LiverMoreStrategy():
 	'''
 	def addToNumberFigure(self, datetime_s , number, i_condition):
 		if i_condition == ShangShenQuShi:
-			self.number_ssqs.append( (datetime_s, number , NoBelowLine))
+			self.number_ssqs.append( (datetime_s, number))
 		if i_condition == ZiRanHuiShen:
-			self.number_zrhs.append( (datetime_s, number , NoBelowLine))
+			self.number_zrhs.append( (datetime_s, number))
 		if i_condition == CiJiHuiShen:
-			self.number_cjhs.append( (datetime_s, number , NoBelowLine))
+			self.number_cjhs.append( (datetime_s, number))
 		if i_condition == XiaJiangQushi:
-			self.number_xjqs.append( (datetime_s, number , NoBelowLine))
+			self.number_xjqs.append( (datetime_s, number))
 		if i_condition == ZiRanHuiChe:
-			self.number_zrhc.append( (datetime_s, number , NoBelowLine))
+			self.number_zrhc.append( (datetime_s, number))
 		if i_condition == CiJiHuiChe :
-			self.number_cjhc.append( (datetime_s, number , NoBelowLine))
+			self.number_cjhc.append( (datetime_s, number))
 
 		color = QianBi
 		if i_condition == ShangShenQuShi:
@@ -109,41 +105,16 @@ class LiverMoreStrategy():
 					self.cp_data.append( (x,y))
 				elif y > py * ( 1 + param1 / 100.0):
 					self.cp_data.append( (x,y))
-
 	'''
-	判断下降趋势
-	'''
-	def judge_xjqs(self , big_condition ,  y , param2):
-		if len(self.number_xjqs) > 0:
-			if y < self.number_xjqs[-1][1]:
-				big_condition = XiaJiangQushi
-		if len(self.number_zrhc) > 0:
-			for i in range(1 , len(self.number_zrhc) + 1):
-				if self.number_zrhc[-i][2] == RED_LINE and y < self.number_zrhc[-i][1] * ( 1 - param2 / 100.0):
-					big_condition = XiaJiangQushi
-		return big_condition
-	'''
-	判断上升趋势
-	'''
-	def judge_ssqs(self , big_condition ,  y , param2):
-		if len(self.number_ssqs) > 0:
-			if y > self.number_ssqs[-1][1]:
-				print "judge ssqs:" + str(y) + "  " + str(self.number_ssqs[-1][0]) + "  " + str(self.number_ssqs[-1][1]) + "  " + str(self.number_ssqs[-1][2]) 
-				big_condition = ShangShenQuShi
-		if len(self.number_zrhs) > 0:
-			for i in range(1 , len(self.number_zrhs) + 1):
-				if self.number_zrhs[-i][2] == BLACK_LINE and y > self.number_zrhs[-i][1] * ( 1 + param2 / 100.0):
-					big_condition = ShangShenQuShi
-		return big_condition
-	'''
-	获得新的K线
 	'''
 	def getNewKline(self , param1 = 6 , param2 = 3 ):
+		MainTrend     = MainShangShen
 		big_condition = ShangShenQuShi     # 当前趋势栏
 		point_color   = HeiMoShui
 
 		start_point   = (self.ori_data[0][0] , self.ori_data[0][1] )
 		if self.ori_data[1][1] > self.ori_data[0][1]:
+			MainTrend     = MainXiaJiang
 			big_condition = XiaJiangQushi
 			point_color   = HongMoShui
 
@@ -164,7 +135,6 @@ class LiverMoreStrategy():
 					# 上个区间结束
 					self.QuJianPairs.append( (start_point, (pl_x,pl_y) , big_condition)) 
 					self.keyPointArr.append( (pl_x,pl_y,RED_LINE))
-					self.number_ssqs[-1] = (self.number_ssqs[-1][0] , self.number_ssqs[-1][1] , RED_LINE)
 					# 下个区间开始
 					big_condition = ZiRanHuiChe
 					start_point = (x,y)
@@ -173,29 +143,39 @@ class LiverMoreStrategy():
 				print "t2:" + ZiRanHuiShen 
 				if y > pl_y:
 					print "y > pl_y"
-					# 自然回升转 上升趋势
-					# 1、大于上升趋势的最后一个点
-					# 2、大于最近的带有黑色线的自然上升点
-					big_condition = self.judge_ssqs(big_condition , y , param2)
+					for i in range(1, min( TimeWindow , len(self.keyPointArr) + 1)):
+						pre_line = self.keyPointArr[-i]
+						# 自然回升转 上升趋势
+						if MainTrend == MainShangShen:
+							if pre_line == RED_LINE :
+								if y > pre_line[1]:
+									big_condition = ShangShenQuShi
+								break
+						elif MainTrend == MainXiaJiang:
+							if pre_line == RED_LINE:
+								if y > pre_line[1] * (1 + param2/100.0):
+									big_condition = ShangShenQuShi
+									MainTrend = MainShangShen
 					self.addToNumberFigure(x , y, big_condition)
 				elif y < pl_y * (1 - param1 / 100.0):
-					print "y < pl_y * (1 - param1 / 100.0) y: "  + str(y) + "  < " + str(pl_y * (1 - param1 / 100.0) )
+					print "y < pl_y * (1 - param1 / 100.0)"
 					self.QuJianPairs.append( (start_point , (pl_x,pl_y) , big_condition))
 					self.keyPointArr.append( (pl_x , pl_y , BLACK_LINE))
-					self.number_zrhs[-1] = (self.number_zrhs[-1][0] , self.number_zrhs[-1][1] , BLACK_LINE)
 					#开启下一个状态
 					big_condition = ZiRanHuiChe
 					start_point = (x,y)
-					# 自然回升转自然回撤
-					# 自然回升转下降趋势
-					# 当前点小于下降趋势的最后一个点 或 小于带红线的自然回撤最后一个点
-					big_condition = self.judge_xjqs(big_condition , y , param2)
-					# 自然回升转次级回撤
-					# 当前价格 > 自然回撤的最后一个价格时，为次级回撤
-					if len(self.number_zrhc) > 0 and y > self.number_zrhc[-1][1]:
+					#判断是否是次级回撤
+					if len(self.number_cjhc) > 0 and y > self.number_cjhc[-1][1]:
 						big_condition = CiJiHuiChe
-					if len(self.number_zrhc) == 0:
-						big_condition = ZiRanHuiChe
+					#判断是否是下降趋势
+					if len(self.number_xjqs) > 0:
+						if MainTrend == MainXiaJiang:
+							if y < self.number_xjqs[-1][1]:
+								big_condition = XiaJiangQushi
+						elif MainTrend == MainShangShen:
+							if y < self.number_xjqs[-1][1] * (1 - param2 / 100.0):
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
 					self.addToNumberFigure(x, y , big_condition)
 			elif big_condition == CiJiHuiShen:
 				print "t3:" + CiJiHuiShen 
@@ -203,26 +183,32 @@ class LiverMoreStrategy():
 					#判断次级回升上升为自然回升
 					if len(self.number_zrhs) > 0 and y > self.number_zrhs[-1][1]:
 						big_condition = ZiRanHuiShen
-					if len(self.number_zrhs) == 0:
-						big_condition = ZiRanHuiShen
-					#判断次级回升为上升趋势
-					big_condition = self.judge_ssqs(big_condition, y, param2)
+					if len(self.number_ssqs) > 0:
+						if MainTrend == MainShangShen:
+							if y > self.number_ssqs[-1][1]:
+								big_condition = ShangShenQuShi
+								MainTrend = MainShangShen
+						elif MainTrend == MainXiaJiang:
+							if y > self.number_ssqs[-1][1] * (1 + param2 / 100.0):
+								big_condition = ShangShenQuShi
+								MainTrend = MainShangShen
 					self.addToNumberFigure( x , y , big_condition) # 上升趋势延续，黑墨水描绘
 				elif y < pl_y * (1 - param1 / 100.0):
 					#结束上一个状态
 					self.QuJianPairs.append((start_point , (pl_x, pl_y) , big_condition))
 					#开启下一个状态
 					## 次级回升这里之后的状态。。 比较难搞，先简单定义为自然回撤
-					# 如果小于自然回撤的最后一个点， 则记录在自然回撤里
-					big_condition = CiJiHuiChe
+					big_condition = ZiRanHuiChe
 					start_point = (x,y)
-					#判断自然回撤
-					if len(self.number_zrhc) > 0 and y < self.number_zrhc[-1][1]:
-						big_condition = ZiRanHuiChe
-					if len(self.number_zrhc) == 0:
-						big_condition = ZiRanHuiChe
-					#判断下降趋势
-					big_condition = self.judge_xjqs(big_condition, y, param2)
+					if len(self.number_xjqs) > 0 :
+						if MainTrend == MainXiaJiang:
+							if y < self.number_xjqs[-1][1]:
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
+						elif MainTrend == MainShangShen:
+							if y < self.number_xjqs[-1][1] * (1 - param2 / 100.0):
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
 					self.addToNumberFigure(x , y, big_condition)
 					## 向下
 			elif big_condition == XiaJiangQushi:
@@ -233,7 +219,6 @@ class LiverMoreStrategy():
 					# 上个区间结束
 					self.QuJianPairs.append( (start_point, (pl_x,pl_y) , big_condition))
 					self.keyPointArr.append( (pl_x,pl_y, BLACK_LINE))
-					self.number_xjqs[-1] = (self.number_xjqs[-1][0], self.number_xjqs[-1][1], BLACK_LINE)
 					# 下个区间开始
 					big_condition = ZiRanHuiShen
 					start_point = (x,y)
@@ -241,51 +226,66 @@ class LiverMoreStrategy():
 			elif big_condition == ZiRanHuiChe:
 				print "t5:" + big_condition
 				if y < pl_y:
-					# 自然回撤转下降趋势
-					big_condition = self.judge_xjqs(big_condition , y , param2)
+					# 需要处理，自然回撤，转变为下降趋势
+					## 如果向下突破前面的  上升趋势终点红线超过3个点
+					for i in range(1,min( TimeWindow, len(self.keyPointArr) + 1)):
+						pre_line = self.keyPointArr[-i]
+						if pre_line[2] == BLACK_LINE:
+							#自然回撤转变为下降趋势
+							if MainTrend == MainShangShen and y < pre_line[1] * (1 - param2 / 100.0):
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
+							if MainTrend == MainXiaJiang and y < pre_line[1]:
+								print pre_line
+								print y
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
+							break
 					self.addToNumberFigure( x , y, big_condition)
 				elif y > pl_y * (1 + param1 / 100.0):
 					# 结束上一个状态
 					self.QuJianPairs.append( (start_point, (pl_x , pl_y) , big_condition))
 					self.keyPointArr.append( (pl_x , pl_y ,RED_LINE))
-					self.number_zrhc[-1] = (self.number_zrhc[-1][0] , self.number_zrhc[-1][1] , RED_LINE)
 					# 开启下一个状态
 					big_condition = ZiRanHuiShen
 					start_point = (x,y)
 					# 判断是否是次级回升
 					if len(self.number_zrhs) > 0 and y < self.number_zrhs[-1][1]:
 						big_condition = CiJiHuiShen
-						print self.number_zrhs
-					if len(self.number_zrhs) == 0:
-						big_condition = ZiRanHuiShen
 					# 判断是否是上升趋势 , 
-					big_condition = self.judge_ssqs(big_condition ,y ,param2)
+					if len(self.number_ssqs) > 0 :
+						if (MainTrend == MainShangShen and y > self.number_ssqs[-1][1]) or (MainTrend == MainXiaJiang and y > self.number_ssqs[-1][1]*(1+param2/100.0)):
+							big_condition = ShangShenQuShi
+							MainTrend = MainShangShen
 					self.addToNumberFigure( x, y , big_condition)
 			elif big_condition == CiJiHuiChe:
 				print "t6:" + big_condition
 				if y < pl_y:
 					##次级回撤变为自然回撤
-					if len(self.number_zrhc) > 0 and y < self.number_zrhc[-1][1]:
-						big_condition = ZiRanHuiChe
-					if len(self.number_zrhc) == 0:
-						big_condition = ZiRanHuiChe
-					# 次级回撤转下降趋势
-					big_condition = self.judge_xjqs(big_condition , y, param2)
+					if len(self.number_cjhc) > 0 and y < self.number_cjhc[-1][1]:
+						big_condition = CiJiHuiChe
+					if len(self.number_xjqs) > 0 :
+						if MainTrend == MainXiaJiang:
+							if y < self.number_xjqs[-1][1]:
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
+						elif MainTrend == MainShangShen:
+							if y < self.number_xjqs[-1][1] * (1 - param2 / 100.0):
+								big_condition = XiaJiangQushi
+								MainTrend = MainXiaJiang
 					self.addToNumberFigure( x, y , big_condition)
 				elif y > pl_y * (1 + param1 / 100.0):
 					# 上个区间结束
 					self.QuJianPairs.append( (start_point, (pl_x,pl_y) , big_condition))
 					#开启下一个状态
 					## 次级回撤这里之后的状态。。 比较难搞，先简单定义为自然回升
-					big_condition = CiJiHuiShen
+					big_condition = ZiRanHuiChe
 					start_point = (x,y)
-					#判断是否是自然回升
-					if len(self.number_zrhs) > 0 and y > self.number_zrhs[-1][1]:
-						big_condition = ZiRanHuiShen
-					if len(self.number_zrhs) == 0:
-						big_condition = ZiRanHuiShen
 					# 判断是否是上升趋势 , 
-					big_condition = self.judge_ssqs(big_condition , y , param2)
+					if len(self.number_ssqs) > 0 :
+						if (MainTrend == MainShangShen and y > self.number_ssqs[-1][1]) or (MainTrend == MainXiaJiang and y > self.number_ssqs[-1][1]*(1+param2/100.0)):
+							big_condition = ShangShenQuShi
+							MainTrend = MainShangShen
 					self.addToNumberFigure(x , y ,big_condition)
 	'''
 	画图并展示
@@ -326,7 +326,7 @@ class LiverMoreStrategy():
 
 		#print self.keyPointArr
 		for (xx,yy,ccolor) in self.keyPointArr:
-			x = [xx - datetime.timedelta(days=15) , xx + datetime.timedelta(days=15)]
+			x = [xx - datetime.timedelta(days=20) , xx + datetime.timedelta(days=20)]
 			y = [yy,yy]
 			#plt.plot(x , y, color = ccolor) 
 			plt.hlines( y[0], x[0], x[1] , colors = ccolor , linestyles = "dashed")
@@ -334,7 +334,7 @@ class LiverMoreStrategy():
 		plt.show()
 
 def test():
-	data = Future.getOneInstrumentDayBetween('RB88','2013-02-01','2016-07-31') # '2015-01-01','2016-06-23',,2013-06-01','2015-01-01 , '2016-06-23','2017-12-31'
+	data = Future.getOneInstrumentDayBetween('RB88','2014-03-01','2016-07-31') # '2015-01-01','2016-06-23',,2013-06-01','2015-01-01 , '2016-06-23','2017-12-31'
 	data = [ (datetime.datetime.strptime(x[0], '%Y-%m-%d'), x[5]) for x in data]
 
 	#print data[:3]
